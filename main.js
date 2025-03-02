757934d7c174692878f628e9b7be0bb975ec0c20
Leandro Chumbita <chumbitadev@gmail.com>
2025-03-02 10:36:36 -0300

    Actualización de las etiquetas head

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ const backdrop = document.getElementById("backdrop");
 /*abre y cierra el icono del menu*/
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
+    backdrop.classList.toggle('active');
 });
 
 /*se cierra el menu por cada opción que toque*/
 for (let i = 0; i < menuOptions.length; i++) {
     const a = menuOptions[i]
     a.addEventListener('click', () => {
-        navbar.classList.toggle('active');
+        closeMenu();
         const menuOptionsActive = document.querySelectorAll("#navbar > a.active");
         for (let j = 0; j < menuOptionsActive.length; j++) {
             menuOptionsActive[j].classList.remove('active');
@@ -22,23 +23,26 @@ for (let i = 0; i < menuOptions.length; i++) {
 }
 
 // Cerrar el menú si se hace clic fuera (en el backdrop)
-backdrop.addEventListener("click", () => {
+backdrop.addEventListener("click", closeMenu);
+
+// Función para cerrar el menú y el backdrop
+function closeMenu() {
     navbar.classList.remove("active");
     backdrop.classList.remove("active");
-  });
-  
-  const form = document.getElementById("form");
-  const customAlert = document.getElementById("customAlert");
-  
-  form.addEventListener("submit", function () {  
-    customAlert.style.display = "block";
-    setTimeout(function () {
-      closeAlert();
-      form.submit();
-    }, 5000);
-  });
-  
-  // Función para cerrar el alert
-  function closeAlert() {
-    customAlert.style.display = "none";
-  }
+}
+
+const form = document.getElementById("form");
+const customAlert = document.getElementById("customAlert");
+
+form.addEventListener("submit", function () {  
+  customAlert.style.display = "block";
+  setTimeout(function () {
+    closeAlert();
+    form.submit();
+  }, 5000);
+});
+
+// Función para cerrar el alert
+function closeAlert() {
+  customAlert.style.display = "none";
+}
